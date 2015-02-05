97cf625622898601d0f61e09384d9b4bf0b8b833
Katrina Sanford <blueyed_qt@hotmail.com>
2015-02-05 13:01:28 -0800

    Users functioning properly, able to add.  Also added validations and errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,27 @@ class UsersController < ApplicationController
   end
 
   def new
+  	@errors = flash[:errors]
   	@user = User.new
   end
 
   def create
+  	@user = User.create(user_params)
+  	if @user.save
+  		# sign in the user
+  		redirect_to @user
+  	else
+  		flash[:errors] = @user.errors.full_messages
+  		redirect_to '/'
+  	end
+
   end
 
   def show
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ActiveRecord::Base
+	attr_accessor :password, :password_confirmation
+	email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+
+	validates :first_name, presence: true, length: { maximum: 30 }
+  	validates :last_name, presence: true, length: { maximum: 30 }
+  	validates :email, presence: true, format: { with: email_regex }, uniqueness: { case_sensitive: false }
+  	validates :password, presence: true, confirmation: true, length: { within: 4..100 }
+
+  	before_save :encrypt_password
+
+
+
+  	private
+  	def encrypt_password
+  		self.salt = Digest::SHA2.hexdigest("#{Time.now.utc}--#{self.password}") if self.new_record?
+  		self.encrypted_password = encrypt(self.password)
+  	end
+  	def encrypt(pass)
+  		Digest::SHA2.hexdigest("#{self.salt}--#{pass}")
+  	end
 end
