d4b27f690d2663a57275ba8e12229c1af498cd05
Katrina Sanford <blueyed_qt@hotmail.com>
2015-02-16 14:02:39 -0800

    Saving posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  def index
+    @post = Post.new
+    @posts = Post.all
+  end
+
+  def new
+  	@posts = Post.all
+  end
+
+  def create
+  	params[:post][:user_id] = session[:user_id]
+  	@post = Post.create(post_params)
+  	if @post.save
+  	else 
+  		flash[:errors] = @post.errors.full_messages
+  	end
+  	redirect_to '/posts/new'
+  end
+
+  def show
+  end
+
+  def post_params
+  	params.require(:post).permit(:message, :user_id)
+  end
+end
